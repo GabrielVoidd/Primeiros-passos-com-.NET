bf6453e5a512dc49707ecea595abde58ec2e423e
GabrielVoidd <souzagabriel0006@gmail.com>
2020-05-27 16:43:17 -0300

    Criando pasta, adicionando anotacoes e primeira aplicacao console

---
+++ Iniciando com .NET/DigitalInnovationOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numeroDeVezes = 10;
+
+            for(int i = 0; i < numeroDeVezes; i++)
+            {
+                System.Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
